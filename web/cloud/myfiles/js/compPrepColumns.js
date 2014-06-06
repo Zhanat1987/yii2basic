52eaae864cd1ba1dc6d9445d891e333a0508a7be
admin <iskakov_zhanat@mail.ru>
2014-06-06 14:31:09 +0600

    last changes

--- web/cloud/myfiles/js/compPrepColumns.js
+++ web/cloud/myfiles/js/compPrepColumns.js
@@ -34,7 +34,7 @@ function columns()
             success: function(response) {
                 $('.columnsM').modal('hide');
                 if (response.status == 'ok') {
-                    reloadPjax(grid);
+                    reloadPjaxCPC(grid);
                     console.log(response.msg);
                 } else if (response.status == 'error') {
                     console.log(response.msg);
@@ -44,63 +44,24 @@ function columns()
         return false;
     });
 }
-function reloadPjax(grid)
+function reloadPjaxCPC(grid)
 {
     if ($('#' + grid + ' tr.filters input:eq(0)').length) {
         $('#' + grid + ' tr.filters input:eq(0)').trigger('change');
     } else if ($('#' + grid + ' tr.filters select:eq(0)').length) {
         $('#' + grid + ' tr.filters select:eq(0)').trigger('change');
     } else {
-//        $.pjax.reload('#' + $('#' + grid).parent().attr('id'),
-//            {
-//                push:true,
-//                replace:false,
-//                timeout:5000,
-//                scrollTo:false,
-//                url: window.location.pathname
-//            }
-//        );
-    }
-}
-
-function reloadPjaxCPC()
-{
-    $(document).on('pjax:complete', function(xhr, textStatus) {
-        $('#' + xhr.target.id + ' tr.filters td:eq(0)').html('<a href="#" class="reloadPjax" ' +
-            'title="Показать все" data-toggle="tooltip">' +
-            '<i class="fa fa-refresh"></i></a>');
-        if ($('select.select2inBox').length) {
-            /**
-             * Uncaught query function not defined for Select2 s2id
-             * http://stackoverflow.com/questions/14483348/
-             * query-function-not-defined-for-select2-undefined-error
-             */
-            $('select.select2inBox').select2();
-        }
-        if ($('select.select2').length) {
-            $('select.select2').select2();
-        }
-        tooltipPopover();
-    });
-    $(document).on('pjax:start', function(xhr, textStatus) {
-        $.pjax.defaults.timeout = 5000;
-    });
-    $('.reloadPjax').live('click', function() {
-        var id = $(this).parents('div[id$="-pjax"]').attr('id');
-        var id2 = $(this).parents('.grid-view').parent().attr('id');
-        $.pjax.reload('#' + id2,
+        $.pjax.reload('#' + $('#' + grid).parent().attr('id'),
             {
-                "push":true,
-                "replace":false,
-                "timeout":5000,
-                "scrollTo":false,
+                push:true,
+                replace:false,
+                timeout:5000,
+                scrollTo:false,
                 url: window.location.pathname
             }
         );
-        return false;
-    });
+    }
 }
 $(document).ready(function() {
     columns();
-    reloadPjaxCPC();
 });
